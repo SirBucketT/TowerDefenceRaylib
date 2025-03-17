24df045c976bd593fc482992dd42f872a4c8ed93
SirBucketT <tkpriva@icloud.com>
2025-03-17 11:49:48 +0100

    trying to draw grid

--- drawGrids.c
+++ drawGrids.c
@@ -8,4 +8,23 @@
 
 DrawGrid() {
 
+    for (int i = 0; i <= COLS; i++) {
+        DrawLine(
+            i * CELL_SIZE,          // startPosX
+            0,                      // startPosY
+            i * CELL_SIZE,          // endPosX
+            SCREEN_HEIGHT,          // endPosY
+            DARKGRAY                // color
+        );
+    }
+
+    for (int i = 0; i <= ROWS; i++) {
+        DrawLine(
+            0,                      // startPosX
+            i * CELL_SIZE,          // startPosY
+            SCREEN_WIDTH,           // endPosX
+            i * CELL_SIZE,          // endPosY
+            DARKGRAY                // color
+        );
+    }
 }
