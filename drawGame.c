785170fbab7283d5841b31dc99d68b3518f211a8
SirBucketT <tkpriva@icloud.com>
2025-03-18 12:39:41 +0100

    .

--- drawGame.c
+++ drawGame.c
@@ -9,7 +9,6 @@ void GridDraw() {
     for (int i = 0; i <= COLS; i++) {
         DrawLine(i * CELL_SIZE, 0, i * CELL_SIZE, SCREEN_HEIGHT, WHITE);
     }
-
     for (int i = 0; i <= ROWS; i++) {
         DrawLine(0, i * CELL_SIZE, SCREEN_WIDTH, i * CELL_SIZE, WHITE);
     }
