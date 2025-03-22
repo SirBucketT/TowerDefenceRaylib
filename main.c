076b993b4314351bbe07f467f01c54c41b0d1046
SirBucketT <tkpriva@icloud.com>
2025-03-22 01:07:30 +0100

    menu implementation

--- main.c
+++ main.c
@@ -3,27 +3,69 @@
 #include "raylib.h"
 #include "screenData.h"
 #include "drawGame.c"
-#include "terrainManager.c"  // Include our new terrain manager
+#include "terrainManager.c"
 
-int gameState = 0;
+//=========================================================================================================================
+//     Game states:
+//     3: Main Menu
+//     0: Wall placement phase
+//     1: Turret placement phase
+//     2: Game running phase
+//=========================================================================================================================
+int gameState = 3;
+
+// Define menu button areas
+Rectangle playButton = { 0 };
+Rectangle quitButton = { 0 };
+
+void DrawMainMenu(void) {
+
+    DrawText("TOWER DEFENCE GAME OR SOMETHING", SCREEN_WIDTH/2 - 300, SCREEN_HEIGHT/4, 30, WHITE);
+
+    playButton = (Rectangle){ SCREEN_WIDTH/2 - 100, SCREEN_HEIGHT/2, 200, 50 };
+    quitButton = (Rectangle){ SCREEN_WIDTH/2 - 100, SCREEN_HEIGHT/2 + 80, 200, 50 };
+
+    DrawRectangleRec(playButton, DARKGRAY);
+    DrawRectangleLinesEx(playButton, 2, WHITE);
+    DrawText("PLAY GAME", playButton.x + 40, playButton.y + 15, 20, WHITE);
+
+    DrawRectangleRec(quitButton, DARKGRAY);
+    DrawRectangleLinesEx(quitButton, 2, WHITE);
+    DrawText("QUIT GAME", quitButton.x + 40, quitButton.y + 15, 20, WHITE);
+
+    Vector2 mousePoint = GetMousePosition();
+
+    if (CheckCollisionPointRec(mousePoint, playButton)) {
+        DrawRectangleRec(playButton, GRAY);
+        DrawRectangleLinesEx(playButton, 2, WHITE);
+        DrawText("PLAY GAME", playButton.x + 40, playButton.y + 15, 20, WHITE);
+
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+            gameState = 0;
+            GenerateRandomTerrain();
+        }
+    }
+
+    if (CheckCollisionPointRec(mousePoint, quitButton)) {
+        DrawRectangleRec(quitButton, GRAY);
+        DrawRectangleLinesEx(quitButton, 2, WHITE);
+        DrawText("QUIT GAME", quitButton.x + 40, quitButton.y + 15, 20, WHITE);
+
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+            CloseWindow();
+        }
+    }
+}
 
 int main(void) {
-    // Initialize the window
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raylib tower defence thing");
     SetTargetFPS(120);
-    GenerateRandomTerrain();
 
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        //=========================================================================================================================
-        //     Game states:
-        //     0: Wall placement phase
-        //     1: Turret placement phase
-        //     2: Game running phase
-        //=========================================================================================================================
 
-        if (IsKeyPressed(KEY_SPACE)) {
+        if (gameState >= 0 && IsKeyPressed(KEY_SPACE)) {
             if (gameState == 0) {
                 gameState++;
             } else if (gameState == 1) {
@@ -39,37 +81,42 @@ int main(void) {
             findPathBFS(0, 0, COLS - 1, ROWS - 1, previewPath, &previewPathLength);
         }
 
-
-        DrawTerrain();
         switch (gameState) {
+
             case 0:
+                DrawTerrain();
                 DrawPreviewPath();
                 HandleWallPlacement();
-            DrawText("WALL PLACEMENT PHASE", SCREEN_WIDTH/2 - 150, 10, 20, WHITE);
-            DrawText("Left click: Add wall | Right click: Remove wall", 10, SCREEN_HEIGHT - 30, 15, WHITE);
-            DrawText("Press SPACE to continue | Press R to regenerate terrain", 10, SCREEN_HEIGHT - 50, 15, WHITE);
-            break;
+                DrawText("WALL PLACEMENT PHASE", SCREEN_WIDTH/2 - 150, 10, 20, WHITE);
+                DrawText("Left click: Add wall | Right click: Remove wall", 10, SCREEN_HEIGHT - 30, 15, WHITE);
+                DrawText("Press SPACE to continue | Press R to regenerate terrain", 10, SCREEN_HEIGHT - 50, 15, WHITE);
+                DrawWalls();
+                break;
+
             case 1:
+                DrawTerrain();
                 DrawPreviewPath();
                 CreateTurret();
-            DrawText("TURRET PLACEMENT PHASE", SCREEN_WIDTH/2 - 150, 10, 20, WHITE);
-            DrawText("Left click: Add turret | Right click: Remove turret", 10, SCREEN_HEIGHT - 30, 15, WHITE);
-            DrawText("Press SPACE to start game | Press R to regenerate terrain", 10, SCREEN_HEIGHT - 50, 15, WHITE);
-            break;
+                DrawText("TURRET PLACEMENT PHASE", SCREEN_WIDTH/2 - 150, 10, 20, WHITE);
+                DrawText("Left click: Add turret | Right click: Remove turret", 10, SCREEN_HEIGHT - 30, 15, WHITE);
+                DrawText("Press SPACE to start game | Press R to regenerate terrain", 10, SCREEN_HEIGHT - 50, 15, WHITE);
+                DrawText("Press ESC to return to menu", 10, SCREEN_HEIGHT - 70, 15, WHITE);
+                DrawWalls();
+                DrawTurrets();
+                break;
+
             case 2:
+                DrawTerrain();
                 SpawnEnemies();
-            UpdateTurrets();
-            UpdateProjectiles();
+                UpdateTurrets();
+                UpdateProjectiles();
+                DrawWalls();
+                DrawTurrets();
+                DrawProjectiles();
+                break;
+            case 3:
+                DrawMainMenu();
             break;
-        }
-
-
-        //GridDraw();
-        DrawWalls();
-        DrawTurrets();
-
-        if (gameState == 2) {
-            DrawProjectiles();
         }
 
         EndDrawing();
